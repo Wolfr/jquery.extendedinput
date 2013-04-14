ed8fd124c76fe09311046a23e94784a3d4a9a4ff
Johan Ronsse <wolf@wolfslittlestore.be>
2013-04-14 14:11:36 +0200

    Make this a plugin

--- jquery.extendedinput.js
+++ jquery.extendedinput.js
@@ -10,127 +10,132 @@
  * http://www.opensource.org/licenses/mit-license.php
 */
 
-$(function(){
+(function( $ ) {
+  $.fn.extendedInput = function(options) {
   
-  var hideClass = 'hide';
+    // Plugin settings
+    var settings = $.extend( {
+      hideClass         : 'hide'
+    }, options);
 
-  /*
-    Pattern: show a given HTML element with data attribute
-  */
+    /*
+      Pattern: show a given HTML element with data attribute
+    */
 
-  $('[data-open-element]').click(function(e) {
-    e.preventDefault();
-    // Read data attribute
-    var value = $(this).attr('data-open-element');
+    $('[data-open-element]').click(function(e) {
+      e.preventDefault();
+      // Read data attribute
+      var value = $(this).attr('data-open-element');
 
-    // Open relevant box and add trigger class to hide box later
-    $('[data-element-to-be-opened="' + value + '"]').removeClass(hideClass).addClass('opened-via-linked-box');
-  });
+      // Open relevant box and add trigger class to hide box later
+      $('[data-element-to-be-opened="' + value + '"]').removeClass(settings.hideClass).addClass('opened-via-linked-box');
+    });
 
-  /*
-    Pattern: show a given container based on radio choice
-    This should be an HTML element with a data attribute of data-show with the same value as the checkbox id
-  */
+    /*
+      Pattern: show a given container based on radio choice
+      This should be an HTML element with a data attribute of data-show with the same value as the checkbox id
+    */
 
-  $('input[type="radio"]').change(function() {
-    // All radios will change, check which one is checked
-    if ($(this).is(':checked')) {
+    $('input[type="radio"]').change(function() {
+      // All radios will change, check which one is checked
+      if ($(this).is(':checked')) {
+
+        // Alternate method through data attribute
+        var showId = 'div[data-show*="' + $(this).attr('id') + '"]';
+
+        // Hide all linked data fields
+
+          // Find out which data fields are linked
+          var radioIDs= [];
+          $(this).closest('ul, table').find('input[type="radio"]').each(function() {
+            radioIDs.push(this.id);
+          });
+
+          $.each(radioIDs, function(index, val) {
+            // Now hide all the ids in array
+            $('div[data-show*='+val+']').addClass(settings.hideClass)
+          });
+
+        // Except the one tied to radio id
+        $(showId).removeClass(settings.hideClass);
+      }
+    });
+
+    /*
+      Pattern: show a given container based checkbox choice
+      This should be an HTML element with a data attribute of data-show with the same value as the checkbox id
+
+      TBD update to bootstrap markup
+    */
+
+    $('input[type="checkbox"]').change(function() {
 
       // Alternate method through data attribute
-      var showId = 'div[data-show*="' + $(this).attr('id') + '"]';
+      var showId = 'div[data-show="' + $(this).attr('id') + '"]';
+
+      // Action on check/uncheck checkbox
+      if ($(this).is(':checked')) {
+        $(showId).removeClass(settings.hideClass);
+      } else {
+        $(showId).addClass(settings.hideClass);
+      }
+    });
+
+    /*
+      Pattern: show a given container based on a selected <option>
+    */
+
+    $('select').change(function(e) {
 
       // Hide all linked data fields
 
         // Find out which data fields are linked
-        var radioIDs= [];
-        $(this).closest('ul, table').find('input[type="radio"]').each(function() {
-          radioIDs.push(this.id);
+        var selectOptionIDs= [];
+        $(this).find('option').each(function() {
+          selectOptionIDs.push(this.id);
         });
-
-        $.each(radioIDs, function(index, val) {
+  
+        $.each(selectOptionIDs, function(index, val) {
           // Now hide all the ids in array
-          $('div[data-show*='+val+']').addClass(hideClass)
+          $('div[data-show*='+val+']').addClass(settings.hideClass)
         });
 
-      // Except the one tied to radio id
-      $(showId).removeClass(hideClass);
-    }
-  });
+      // Except the one tied to data attribute
+      var showId = 'div[data-show*="' + $(this).find('option:selected').attr('id') + '"]';
+      $(showId).removeClass(settings.hideClass);
 
-  /*
-    Pattern: show a given container based checkbox choice
-    This should be an HTML element with a data attribute of data-show with the same value as the checkbox id
-
-    TBD update to bootstrap markup
-  */
-
-  $('input[type="checkbox"]').change(function() {
-
-    // Alternate method through data attribute
-    var showId = 'div[data-show="' + $(this).attr('id') + '"]';
-
-    // Action on check/uncheck checkbox
-    if ($(this).is(':checked')) {
-      $(showId).removeClass(hideClass);
-    } else {
-      $(showId).addClass(hideClass);
-    }
-  });
-
-  /*
-    Pattern: show a given container based on a selected <option>
-  */
-
-  $('select').change(function(e) {
-
-    // Hide all linked data fields
-  
-      // Find out which data fields are linked
-      var selectOptionIDs= [];
-      $(this).find('option').each(function() {
-        selectOptionIDs.push(this.id);
-      });
-    
-      $.each(selectOptionIDs, function(index, val) {
-        // Now hide all the ids in array
-        $('div[data-show*='+val+']').addClass(hideClass)
-      });
-
-    // Except the one tied to data attribute
-    var showId = 'div[data-show*="' + $(this).find('option:selected').attr('id') + '"]';
-    $(showId).removeClass(hideClass);
-
-  });
-
-  /*
-    Data toggle
-    Toggles between 2 sets of data using 2 links as controls
-  */
-
-  $('[data-toggle="true"] a').click(function(e) {
-
-    e.preventDefault();
-
-    // Manage active state
-    $(this).siblings('a').removeClass('active');
-    $(this).addClass('active');
-
-    // Store our references
-    var data = [];
-    $(this).parent().find('a[data-toggle-trigger]').each(function() {
-      data.push($(this).attr('data-toggle-trigger'));
     });
 
-    // Hide all referenced fields
-    $.each(data, function(index, val) {
-      $('[data-toggle-element*='+val+']').addClass(hideClass)
-    });
-    
-    // Except the one tied to data attribute
-    var showData = '[data-toggle-element="' + $(this).attr('data-toggle-trigger') +  '"]';
-    $(showData).removeClass(hideClass);
+    /*
+      Data toggle
+      Toggles between 2 sets of data using 2 links as controls
+    */
 
-  });
+    $('[data-toggle="true"] a').click(function(e) {
+
+      e.preventDefault();
+
+      // Manage active state
+      $(this).siblings('a').removeClass('active');
+      $(this).addClass('active');
+
+      // Store our references
+      var data = [];
+      $(this).parent().find('a[data-toggle-trigger]').each(function() {
+        data.push($(this).attr('data-toggle-trigger'));
+      });
+
+      // Hide all referenced fields
+      $.each(data, function(index, val) {
+        $('[data-toggle-element*='+val+']').addClass(settings.hideClass)
+      });
   
+      // Except the one tied to data attribute
+      var showData = '[data-toggle-element="' + $(this).attr('data-toggle-trigger') +  '"]';
+      $(showData).removeClass(settings.hideClass);
 
-});
+
+    });
+
+  };
+})( jQuery );
---
+++ scripts.js
@@ -0,0 +1,9 @@
+(function($){
+  
+  // Invoke our plugin with default options exposed
+  $('body').extendedInput({
+    hideClass: 'hidden'
+  });
+  // You could also write $('body').extendedInput(); if the defaults are fine
+  
+})(jQuery);
